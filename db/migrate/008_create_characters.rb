a42e05062b8c1461b931c67a91c177097e686ee1
taroj khan <tarujkhan@yahoo.com>
2020-04-13 14:49:14 +0000

    Done.

--- db/migrate/008_create_characters.rb
+++ db/migrate/008_create_characters.rb
@@ -1,6 +1,6 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
 def change
-    create_table :actors do |t|
+    create_table :characters do |t|
       t.string :name
       t.integer :actor_id
       t.integer :show_id
