1822e8971f3f13c2272831676a03ccf38d21eedc
taroj khan <tarujkhan@yahoo.com>
2020-04-13 19:14:12 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,8 @@
 class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
-
-   has_many :actors, through: :characters
+  has_many :actors, through: :characters
+  
   def actors_list 
     self.actors.each do |actor|
    "#{self.first_name} #{self.last_name}"
