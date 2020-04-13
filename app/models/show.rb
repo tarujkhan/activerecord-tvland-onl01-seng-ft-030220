059314a4b1fae3f633b87ba3505298621cdacc28
taroj khan <tarujkhan@yahoo.com>
2020-04-13 15:27:21 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
+  
+  def actors_list 
+    self.actors.each do |actor|
+   "#{self.first_name} #{self.last_name}"
 end
+end 
+end 
