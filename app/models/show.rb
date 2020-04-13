3923ae7ae2f8202be3bfc61a610d08da9114a239
taroj khan <tarujkhan@yahoo.com>
2020-04-13 19:14:28 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   
   def actors_list 
     self.actors.each do |actor|
-   "#{self.first_name} #{self.last_name}"
-end
-end 
+      "#{self.first_name} #{self.last_name}"
+    end
+  end 
 end 
